0b12a3325ba83b73caf510b7404715465d479d9c
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-04-03 18:25:59 -0400

    Project started

---
+++ src/components/counter.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
+}
+
+export default Counter;
