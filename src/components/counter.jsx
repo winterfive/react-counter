7cae1edd2ae5621b6bcdf852c06125a598b60881
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-04-04 10:21:32 -0400

    Adding styles prop

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,12 +5,18 @@ class Counter extends Component {
     count: 0,
     imageUrl: "https://picsum.photos/300"
   };
+
+  styles = {
+    fontSize: 80
+  };
+
   render() {
     return (
       <div>
-        <img className="badge badge-primary" src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={{ fontSize: 50 }} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-lg">Increment</button>
       </div>
     );
   }
